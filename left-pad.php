b39061d37482dbaf7d59d93710f4a4b6aaa949c6
Max Zhuk <zhukmax@ya.ru>
2018-04-23 13:04:28 +0300

    Update left-pad.php

--- left-pad.php
+++ left-pad.php
@@ -1,16 +1,16 @@
 <?php
-trait LeftPadTrait {
-  public function leftpad ($str, $len, $ch)
+
+class LeftPad {
+  public static function leftpad($str, int $len, string $ch) : string
   {
-    $str = (string)$str;
+       $str = (string)$str;
   
-    if (!$ch) $ch = ' ';
-    $len = $len - strlen($str);
-    for ($i = 0; $len > $i; $i++)
-    {
-      $str = $ch . $str;
-    }
-    return $str;
+       if (!$ch) $ch = ' ';
+       $len = $len - strlen($str);
+       for ($i = 0; $len > $i; $i++)
+       {
+            $str = $ch . $str;
+        }
+        return $str;
   }
 }
-?>
